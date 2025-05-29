5edfb541920ca969682636ea79112ae92849d58f
Walid AlDari <walidaldari1@gmail.com>
2025-05-28 21:48:25 -0400

    Setup basic dashboard skeleton layout.

--- frontend/reviewlensfrontend/src/App.tsx
+++ frontend/reviewlensfrontend/src/App.tsx
@@ -1,10 +1,65 @@
 import { useState } from "react";
 import Navbar from "./components/custom/Navbar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function App() {
   return (
     <>
       <Navbar />
+
+      <div className="p-7">
+        <div>
+          <h1>Dashboard</h1>
+        </div>
+        <div>
+          <Tabs>
+            <TabsList>
+              <TabsTrigger value="Overview">Overview</TabsTrigger>
+              <TabsTrigger value="Reviews" disabled>
+                Reviews
+              </TabsTrigger>
+              <TabsTrigger value="Topics" disabled>
+                Topics
+              </TabsTrigger>
+              <TabsTrigger value="Chatbot" disabled>
+                Chatbot
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="Overview">
+              <div className="grid grid-cols-2 gap-8">
+                <div>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>Box 1</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <p>Graph is here</p>
+                    </CardContent>
+                  </Card>
+                </div>
+                <div>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>Box 2</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <p>Graph is here</p>
+                    </CardContent>
+                  </Card>
+                </div>
+              </div>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
     </>
   );
 }
